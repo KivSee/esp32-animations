99fad8c48d617ce52e7fe8c5ce886aa0c76b6b51
Oren Bigler <bigoreo@gmail.com>
2025-06-13 13:01:58 +0300

    fix: add empty secrets.h instead of template

--- include/secrets.h
+++ include/secrets.h
@@ -4,11 +4,11 @@
 #include <Arduino.h>
 
 /*
-Usage: Copy this file to include/secrets.h, and fill the values set as 'xxxx'.
+Usage: Update the values set as 'xxxx'.
 */
 
 #define SSID "xxxx"
-#define WIFI_PASSWORD "xxxx"
+#define WIFI_PASSWORD "xxxx"    // for no password, use ""
 #define COLOR_ORDER NeoRgbFeature   // can also be NeoGrbFeature
 #define TIME_SERVER_PORT 123
 
